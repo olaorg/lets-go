054792355d66ac1095a691c4762ba0e254a423de
Ibrahim Olanigan <salalite@gmail.com>
2023-04-20 14:25:57 -0400

    Hello and Bye

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+import "log"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello world received a request.")
+		w.Write([]byte("Hello, world!"))
+	})
+
+	http.HandleFunc("/bye", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Panic received a request.")
+		w.Write([]byte("Goodbye, fellows!"))
+	})
+
+	http.ListenAndServe(":9090", nil)
+}
